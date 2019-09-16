555c5f4957d94e9889dab5a91b52e6842341b6e6
Alex Regan <alex.joseph.regan@gmail.com>
2019-09-16 11:50:39 -0700

    feat: testing Handler interface

--- __tests__/Application.spec.ts
+++ __tests__/Application.spec.ts
@@ -3,11 +3,19 @@ import request from "supertest";
 import { Application, isHTTPHandler, StatusCode } from "../src/main";
 
 describe("Application", () => {
-  it("should be a Handler", () => {
+  it("should implement Handler", () => {
     let app = new Application();
 
     expect(app).toBeInstanceOf(Application);
+
     expect(isHTTPHandler(app)).toBe(true);
+    expect(isHTTPHandler(app.router)).toBe(true);
+
+    expect(isHTTPHandler({})).toBe(false);
+    expect(isHTTPHandler(null)).toBe(false);
+    expect(isHTTPHandler(undefined)).toBe(false);
+    expect(isHTTPHandler([])).toBe(false);
+    expect(isHTTPHandler({ serveHTTP: "" })).toBe(false);
   });
 
   it("should handle a request", async () => {
